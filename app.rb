9d9a0dd5471fc595672937b70fa414e1a9e1be37
Mxrshy <mxrshy@mxrshy.com>
2018-10-19 00:53:31 +0200

    Là tout marche mais faut encore commenter et retaper en mode Jacky Tuning là

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require_relative 'lib/app/townhalls_scrapper'
 require_relative 'lib/app/townhalls_mailer'
 require_relative 'lib/app/townhalls_follower'
 
-#Scrapp_All.new.launch
+Scrapp_All.new.launch
 Mailer.new.perform
 Follower.new.search_all_handle
